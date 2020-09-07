0961799569d538769965cc6d84a35b192d55420c
Magnus Sagerup <magnus.sagerup@gmail.com>
2020-09-07 20:24:45 +0100

    understanding column and rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  home: GymIntro()
 ));
 
 
-class Home extends StatelessWidget {
+class GymIntro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Hello Gym-O trons',
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.black54,
-              fontFamily: 'Syne'
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-              child: Image.asset('assets/gym3.jpg')
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.cyan,
-              child: Text('One'),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.pinkAccent,
-              child: Text('Two'),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.amber,
-              child: Text('Three'),
-            ),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('Click'),
-        backgroundColor: Colors.red[500],
-      ),
+      backgroundColor: Colors.grey[900],
+     body: SafeArea(
+       child: Container(
+         child: Column(
+           crossAxisAlignment: CrossAxisAlignment.stretch,
+           children: <Widget>[
+             Container(
+               color: Colors.pinkAccent,
+               child: (Text('Hello')),
+             ),
+             Container(
+               color: Colors.pinkAccent,
+               child: (Text('new line')),
+             ),
+             Container(
+               color: Colors.pinkAccent,
+               child: (Text('new line again')),
+             ),
+             Container(
+               color: Colors.blue,
+               child: Row(
+                 children: <Widget>[
+                   Text('inline'),
+                   Text('inline'),
+                   Column(
+                     children: <Widget>[
+                       Text('dfsf'),
+                       Text('fdsfd')
+                     ],
+                   )
+                 ],
+               )
+             ),
+           ],
+         )
+       )
+     )
     );
   }
 }
